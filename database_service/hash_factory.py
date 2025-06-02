9a7b3f89f9cc8f602791589987a3a738524993e8
Wahid Shakib <wsakib87@gmail.com>
2025-06-02 10:11:17 +0600

    updated hash factory to support hash rings

--- database_service/hash_factory.py
+++ database_service/hash_factory.py
@@ -1,12 +1,42 @@
+from typing_extensions import Self
 from database_service.abc_classes import DatabaseABC
-from database_service.mysql_service import MySQLServiceSingleton
+import bisect
 
 class HashFactory:
-    def __init__(self, hash_algorithm = None, databases = None, hash_rings = None):
-        self.hash_algorithm = hash_algorithm
-        self.databases = databases or []
-        self.hash_rings = hash_rings or {}
+    def __init__(self, number_of_slots = 10000):
+        self.hash_rings: list[DatabaseABC | None] = [None for _ in range(number_of_slots)]
+        self.number_of_slots = number_of_slots
     
-    def get_db_by_key(self, key: str) -> 'DatabaseABC':
-        return MySQLServiceSingleton()  # For simplicity, returning MySQLServiceSingleton directly.
-        # In a real implementation, we need to use the hash_algorithm to determine which database to return.
+    def get_database_by_key(self, key: str) -> 'DatabaseABC':
+        index = hash(key) % self.number_of_slots
+        database = self.hash_rings[index]
+        if database is not None:
+            return database
+        for i in range(self.number_of_slots):
+            next_index = (index + i) % self.number_of_slots
+            database = self.hash_rings[next_index]
+            if database is not None:
+                return database
+        raise ValueError('No database found for the given key')
+        
+    def add_database(self, database: DatabaseABC):
+        database_index = hash(database) % self.number_of_slots
+        if self.hash_rings[database_index] is None:
+            self.hash_rings[database_index] = database
+
+        raise ValueError('Database already exists in the hash ring')
+
+    def remove_database(self, database: DatabaseABC):
+        database_index = hash(database) % self.number_of_slots
+        if self.hash_rings[database_index] == database:
+            self.hash_rings[database_index] = None
+
+        raise ValueError('Database not found in the hash ring')
+
+class HashFactorySingleton:
+    _instance = None
+
+    def __new__(cls) -> 'HashFactory':
+        if cls._instance is None:
+            cls._instance = HashFactory()
+        return cls._instance
--- database_service/service.py
+++ database_service/service.py
@@ -3,12 +3,12 @@ from database_service.mysql_service import MySQLServiceSingleton
 from sqlalchemy.ext.declarative import DeclarativeMeta
 from pydantic import BaseModel
 from common import CommonQueryModel
-from hash_factory import HashFactory
+from hash_factory import HashFactorySingleton, HashFactory
 
 class DatabaseService:
-    def __init__(self, schema: DeclarativeMeta):
+    def __init__(self, schema: DeclarativeMeta, hash_factory: HashFactory | None = None):
         self.schema = schema
-        self.hash_factory = HashFactory()
+        self.hash_factory = hash_factory or HashFactorySingleton()
 
     async def connect(self):
         databases: list[DatabaseABC] = [
@@ -25,7 +25,7 @@ class DatabaseService:
             await database.disconnect()
     
     async def get_one(self, id: str):
-        database: DatabaseABC = self.hash_factory.get_db_by_key(id)
+        database: DatabaseABC = self.hash_factory.get_database_by_key(id)
         return await database.get_one(id, self.schema)
     
     async def get_all(self, query: CommonQueryModel):
@@ -39,13 +39,13 @@ class DatabaseService:
     
     async def create_one(self, data: BaseModel):
         id: str = 'test' # This should be replaced with a real ID generation logic.
-        database: DatabaseABC = self.hash_factory.get_db_by_key(id)
+        database: DatabaseABC = self.hash_factory.get_database_by_key(id)
         return await database.create_one(data, self.schema)
     
     async def update_one(self, id: str, data: BaseModel):
-        database: DatabaseABC = self.hash_factory.get_db_by_key(id)
+        database: DatabaseABC = self.hash_factory.get_database_by_key(id)
         return await database.update_one(id, data, self.schema)
     
     async def delete_one(self, id: str):
-        database: DatabaseABC = self.hash_factory.get_db_by_key(id)
+        database: DatabaseABC = self.hash_factory.get_database_by_key(id)
         return await database.delete_one(id, self.schema)
